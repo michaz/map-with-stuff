4707297e37a3ca03aef5af5b2cafebd9bdbf196a
Michael Zilske <michael.zilske@tu-berlin.de>
2013-07-29 18:14:02 +0200

    distinguish below and above ground hydrants

--- MyApplication2/src/main/java/com/example/myapplication2/MainActivity.java
+++ MyApplication2/src/main/java/com/example/myapplication2/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -26,17 +28,25 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Activity {
 
     private GoogleMap mMap;
+    private BitmapDescriptor mHydrantMarker;
+    private BitmapDescriptor mAboveGround;
+    private BitmapDescriptor mBelowGround;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpMapIfNeeded();
+        mHydrantMarker = BitmapDescriptorFactory.fromResource(R.drawable.hydrant);
+        mAboveGround = BitmapDescriptorFactory.fromResource(R.drawable.above_ground);
+        mBelowGround = BitmapDescriptorFactory.fromResource(R.drawable.below_ground);
     }
 
     @Override
@@ -69,11 +79,20 @@ public class MainActivity extends Activity {
         }
     }
 
-    private class DownloadHydrantsTask extends AsyncTask<Void, Void, List<LatLng>> {
+    private static class Hydrant {
+        private final String type;
+        private final LatLng latLng;
+        public Hydrant(LatLng latLng, String s) {
+            this.latLng = latLng;
+            this.type = s;
+        }
+    }
+
+    private class DownloadHydrantsTask extends AsyncTask<Void, Void, List<Hydrant>> {
 
         @Override
-        protected List<LatLng> doInBackground(Void... v) {
-            List<LatLng> hydrants = new ArrayList<LatLng>();
+        protected List<Hydrant> doInBackground(Void... v) {
+            List<Hydrant> hydrants = new ArrayList<Hydrant>();
             try {
                 hydrants = getHydrants();
             } catch (IOException e) {
@@ -85,16 +104,25 @@ public class MainActivity extends Activity {
         }
 
         @Override
-        protected void onPostExecute(List<LatLng> hydrants) {
-            for (LatLng position : hydrants) {
+        protected void onPostExecute(List<Hydrant> hydrants) {
+            for (Hydrant hydrant : hydrants) {
+                BitmapDescriptor icon;
+                if ("underground".equals(hydrant.type)) {
+                    icon = mBelowGround;
+                } else if ("pillar".equals(hydrant.type)) {
+                    icon = mAboveGround;
+                } else {
+                    icon = mHydrantMarker;
+                }
                 mMap.addMarker(new MarkerOptions()
-                        .position(position));
+                        .position(hydrant.latLng)
+                        .icon(icon));
             }
         }
     }
 
 
-    public static List getHydrants() throws IOException, IllegalStateException, XmlPullParserException {
+    public static List<Hydrant> getHydrants() throws IOException, IllegalStateException, XmlPullParserException {
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("http://overpass-api.de/api/interpreter");
         try {
@@ -104,7 +132,7 @@ public class MainActivity extends Activity {
             HttpResponse response = httpclient.execute(httppost);
             System.out.println(response.toString());
             HttpEntity responseEntity = response.getEntity();
-            List<LatLng> result = parse(responseEntity.getContent());
+            List<Hydrant> result = parse(responseEntity.getContent());
             return result;
         } catch (ClientProtocolException e) {
             throw new RuntimeException(e);
@@ -163,20 +191,34 @@ public class MainActivity extends Activity {
         }
     }
 
-    private static LatLng readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+    private static Hydrant readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
         parser.require(XmlPullParser.START_TAG, null, "node");
         String lat = parser.getAttributeValue(null, "lat");
         String lon = parser.getAttributeValue(null, "lon");
-
+        Map<String, String> tags = new HashMap<String, String>();
         while (parser.next() != XmlPullParser.END_TAG) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
                 continue;
             }
-            skip(parser);
+            String name = parser.getName();
+            if (name.equals("tag")) {
+                readTag(tags, parser);
+            } else {
+                skip(parser);
+            }
         }
+        return new Hydrant(new LatLng(Double.parseDouble(lat), Double.parseDouble(lon)), tags.get("fire_hydrant:type"));
 
-        return new LatLng(Double.parseDouble(lat), Double.parseDouble(lon));
+    }
 
+    private static void readTag(Map<String, String> tags, XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, "tag");
+        String k = parser.getAttributeValue(null, "k");
+        String v = parser.getAttributeValue(null, "v");
+        tags.put(k, v);
+        while (parser.next() != XmlPullParser.END_TAG) {
+            continue;
+        }
     }
 
 
