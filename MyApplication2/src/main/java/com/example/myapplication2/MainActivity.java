6a3a21b60d4b090781b9b063b3138fbc82590234
Michael Zilske <michael.zilske@tu-berlin.de>
2013-08-01 20:30:50 +0200

    Also fire stations which are areas (ways)

--- MyApplication2/src/main/java/com/example/myapplication2/MainActivity.java
+++ MyApplication2/src/main/java/com/example/myapplication2/MainActivity.java
@@ -141,6 +141,7 @@ public class MainActivity extends Activity {
             List<FireStation> hydrants = new ArrayList<FireStation>();
             try {
                 hydrants = getFireStations();
+                hydrants.addAll(getFireStationsWays());
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (XmlPullParserException e) {
@@ -166,6 +167,25 @@ public class MainActivity extends Activity {
         try {
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
             nameValuePairs.add(new BasicNameValuePair("data", "node[\"amenity\"=\"fire_station\"]"+ BOUNDING_BOX +";out;"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            System.out.println(response.toString());
+            HttpEntity responseEntity = response.getEntity();
+            List<FireStation> result = parse(responseEntity.getContent());
+            return result;
+        } catch (ClientProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<FireStation> getFireStationsWays() throws IOException, IllegalStateException, XmlPullParserException {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://overpass-api.de/api/interpreter");
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("data", "way[\"amenity\"=\"fire_station\"]"+ BOUNDING_BOX +";(._;>;);out;"));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
             HttpResponse response = httpclient.execute(httppost);
             System.out.println(response.toString());
@@ -214,6 +234,7 @@ public class MainActivity extends Activity {
 
     private static List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
         List entries = new ArrayList();
+        Map<String, LatLng> nodes = new HashMap<String, LatLng>();
         String ns = null;
         parser.require(XmlPullParser.START_TAG, ns, "osm");
         while (parser.next() != XmlPullParser.END_TAG) {
@@ -223,7 +244,13 @@ public class MainActivity extends Activity {
             String name = parser.getName();
             // Starts by looking for the entry tag
             if (name.equals("node")) {
-                entries.add(readEntry(parser));
+                Object node = readEntry(parser, nodes);
+                if (node != null) {
+                    entries.add(node);
+                }
+            } else if (name.equals("way")) {
+                FireStation way = readWay(parser, nodes);
+                entries.add(way);
             } else {
                 skip(parser);
             }
@@ -248,11 +275,47 @@ public class MainActivity extends Activity {
         }
     }
 
-    private static Object readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+    private static FireStation readWay(XmlPullParser parser, Map<String, LatLng> nodes) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, null, "way");
+        double latitude = 0.0;
+        double longitude = 0.0;
+        int nNd = 0;
+        Map<String, String> tags = new HashMap<String, String>();
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("tag")) {
+                readTag(tags, parser);
+            } else if (name.equals("nd")) {
+                String k = parser.getAttributeValue(null, "ref");
+                LatLng nd = nodes.get(k);
+                latitude += nd.latitude;
+                longitude += nd.longitude;
+                ++nNd;
+                while (parser.next() != XmlPullParser.END_TAG) {
+                    continue;
+                }
+            } else {
+                skip(parser);
+            }
+        }
+        if ("fire_station".equals(tags.get("amenity"))) {
+            return new FireStation(new LatLng(latitude / nNd, longitude / nNd));
+        } else {
+            throw new RuntimeException("Strange thing.");
+        }
+    }
+
+    private static Object readEntry(XmlPullParser parser, Map<String, LatLng> nodes) throws XmlPullParserException, IOException {
         parser.require(XmlPullParser.START_TAG, null, "node");
+        String id = parser.getAttributeValue(null, "id");
         String lat = parser.getAttributeValue(null, "lat");
         String lon = parser.getAttributeValue(null, "lon");
+        LatLng latLng = new LatLng(Double.parseDouble(lat), Double.parseDouble(lon));
         Map<String, String> tags = new HashMap<String, String>();
+        nodes.put(id, latLng);
         while (parser.next() != XmlPullParser.END_TAG) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
                 continue;
@@ -265,13 +328,12 @@ public class MainActivity extends Activity {
             }
         }
         if ("fire_hydrant".equals(tags.get("emergency"))) {
-            return new Hydrant(new LatLng(Double.parseDouble(lat), Double.parseDouble(lon)), tags.get("fire_hydrant:type"));
+            return new Hydrant(latLng, tags.get("fire_hydrant:type"));
         } else if ("fire_station".equals(tags.get("amenity"))) {
-            return new FireStation(new LatLng(Double.parseDouble(lat), Double.parseDouble(lon)));
+            return new FireStation(latLng);
         } else {
-            throw new RuntimeException("Strange thing.");
+            return null;
         }
-
     }
 
     private static void readTag(Map<String, String> tags, XmlPullParser parser) throws IOException, XmlPullParserException {
